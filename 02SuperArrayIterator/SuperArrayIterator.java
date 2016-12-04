3d5608e5037dacd5d0a5e59f80b4f75be9e47800
JonahSW <jsachsw@gmail.com>
2016-12-04 17:10:10 -0500

    Initial structure of superarray iterator

---
+++ 02SuperArrayIterator/SuperArrayIterator.java
@@ -0,0 +1,18 @@
+import java.util.iterator;
+
+public class SuperArrayIterator implements Iterator<String>{
+
+
+    //hasNext function:
+    public boolean hasNext(){
+	int i = 0;
+	if(i < size() - 1){
+	}
+    }
+
+    //next function:
+    public String next(){
+
+    }
+
+}
