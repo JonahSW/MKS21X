2c34cdd72a152212f9bab8c928ac0e5f29dfd97d
JonahSW <jsachsw@gmail.com>
2016-12-04 21:29:55 -0500

    Continuing to fix Driver.java. Everything compiles but add function will not work for StringySuperArray data1

--- 02SuperArrayIterator/Driver.java
+++ 02SuperArrayIterator/Driver.java
@@ -1,26 +1,35 @@
 public class Driver{
   public static void main(String[] args) {
-    StringySuperArray data = new StringySuperArray();
+    StringySuperArray data1 = new StringySuperArray();
+
+    System.out.println(data1.toStringDebug());
+    
     int i = 0;
     while(i < 26){
 	//data.add(""+(char)('A'+i%26));
-	data.add("asd");
+	data1.add("asd");
 	i++;
     }
 
-    System.out.println(data.add("wfd"));
+    data1.add("hi");
+    
+    System.out.println(data1.add("wfd"));
 
-    System.out.println(data.toStringDebug());
+    System.out.println(data1.size());
+    
+    //System.out.println(data1.get(0));
 
-    System.out.println(data);
+    System.out.println(data1.toStringDebug());
+
+    System.out.println(data1);
     System.out.println("Standard loop:");
 
-    for(int n = 0; n < data.size(); n++){
-      System.out.print(data.get(n)+" ");
+    for(int n = 0; n < data1.size(); n++){
+      System.out.print(data1.get(n)+" ");
     }
     System.out.println();
     System.out.println("for-each loop:");
-    for(String s : data){
+    for(String s : data1){
       System.out.print(s+" ");
     }
   }
